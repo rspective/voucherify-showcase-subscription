cbccb6daa8323be29c25b40aac455add34d2e93b
tpindel <pindel.tomasz@gmail.com>
2016-05-31 15:46:05 +0200

    skeleton of express app

---
+++ src/config.js
@@ -0,0 +1,17 @@
+var _               = require('lodash');
+var path            = require('path');
+var rootPath        = path.normalize(__dirname + '/..');
+var fs              = require('fs');
+
+var cfg_path  = path.join(__dirname ,'./config-' + process.env.NODE_ENV  + '.js');
+var cfg       = fs.existsSync(cfg_path) ? require(cfg_path) : {};
+
+module.exports = _.merge(
+    {
+        env: 'production',
+        port: process.env.PORT || 8080,
+        voucherify: {
+            applicationId: "placeholder",
+            clientSecretKey: "placeholder"
+        }
+    }, cfg);
---
+++ src/server.js
@@ -0,0 +1,16 @@
+var express         = require('express');
+
+var config          = require('./config');
+var port            = process.env.PORT || config.port;
+var cfg             = require(__dirname + "/config");
+var path            = require('path');
+
+var app             = express();
+
+app.get("/ping", function(req, res) {
+    res.status(200).end("Ping");
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port %s!', port);
+});
